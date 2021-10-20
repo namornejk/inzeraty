5337b3b01a3a2cdc2be0c61b345268c35b48b6b8
Tomas Kudrna <tomas.kudrna@unicorn.eu>
2021-10-20 11:25:02 +0200

    Init commit.

---
+++ inzeraty/src/main/java/cz/uhk/ppro/inzeraty/inzeraty/InzeratyApplication.java
@@ -0,0 +1,13 @@
+package cz.uhk.ppro.inzeraty.inzeraty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InzeratyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InzeratyApplication.class, args);
+	}
+
+}
---
+++ inzeraty/src/test/java/cz/uhk/ppro/inzeraty/inzeraty/InzeratyApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.ppro.inzeraty.inzeraty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InzeratyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
